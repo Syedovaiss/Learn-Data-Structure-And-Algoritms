a807e55a8bf8419906e32bd51da219ca7edd6899
Syed Ovais Akhtar <ovais.akhtar@venturedive.com>
2025-02-05 21:21:20 +0500

    Added some more questions

--- src/arrays/Arrays.kt
+++ src/arrays/Arrays.kt
@@ -40,21 +40,150 @@ fun findMaximumTripletProduct(array: Array<Int>): Int {
     }
     return result
 }
+
 /**
 Given a binary array, find the count of a maximum number of consecutive 1s present in the array.
  * **/
 
 fun findMaximumConsecutiveOnes(array: Array<Int>): Int {
-    if(array.contains(1).not()) {
+    if (array.contains(1).not()) {
         return -1
     }
     var count = 0
     for (item in array) {
-        if(item == 1) {
+        if (item == 1) {
             count++
         } else {
             count = 0
         }
     }
     return count
+}
+
+fun isPalindrome(word: String): Boolean {
+    var start = 0
+    var end = word.length - 1
+    while (start < end) {
+        if (word[start] != word[end]) {
+            return false
+        }
+        start++
+        end--
+    }
+    return true
+}
+
+fun isNearlyPalindrome(word: String): Boolean {
+    var isPreviouslyEscaped = false
+    var start = 0
+    var end = word.length - 1
+    while (start < end) {
+        if (word[start] != word[end]) {
+            if (word[start] == word[end - 1] && isPreviouslyEscaped.not()) {
+                isPreviouslyEscaped = true
+                end--
+            } else if (word[start + 1] == word[end] && isPreviouslyEscaped.not()) {
+                isPreviouslyEscaped = true
+                start++
+            } else {
+                return false
+            }
+        }
+        start++
+        end--
+    }
+    return true
+}
+
+fun findMax(items: IntArray): Int {
+    if (items.isEmpty()) return -1
+    var max = items.first()
+    items.forEach { item ->
+        if (item > max) {
+            max = item
+        }
+    }
+    return max
+}
+
+fun findSum(items: IntArray): Int {
+    if (items.isEmpty()) return -1
+    var sum = 0
+    items.forEach {
+        sum += it
+    }
+    return sum
+}
+
+fun reverseArray(array: IntArray): IntArray {
+    if (array.isEmpty()) return intArrayOf()
+    val reversed = arrayListOf<Int>()
+    for (index in array.size - 1 downTo 0) {
+        reversed.add(array[index])
+    }
+    return reversed.toIntArray()
+}
+
+fun containsDuplicate(array: IntArray): Boolean {
+    if (array.isEmpty()) return false
+    val currentArray = arrayListOf<Int>()
+    array.forEach {
+        if (currentArray.contains(it)) {
+            return true
+        }
+        currentArray.add(it)
+    }
+    return false
+}
+
+fun removeDuplicate(array: IntArray): ArrayList<Int> {
+    if (array.isEmpty()) return arrayListOf()
+    val result = arrayListOf<Int>()
+    array.forEach {
+        if (result.contains(it).not()) {
+            result.add(it)
+        }
+    }
+    return result
+}
+
+//Problem: Write a function that rotates the array to the right by k steps.
+fun rotateAnArray(array: Array<Int>, k: Int): ArrayList<Int> {
+    if (array.isEmpty()) return arrayListOf()
+    var startStep = k + 1
+    val result = arrayListOf<Int>()
+    while (startStep != array.size) {
+        result.add(array[startStep])
+        startStep++
+    }
+    for (index in 0..k) {
+        result.add(array[index])
+    }
+    return result
+}
+
+fun findFirstMissingNumberInSequence(array: Array<Int>, size: Int): Int {
+    if (array.isEmpty() && size == 0) return -1
+    if (array.isEmpty() && size == 1) return 1
+    var result = 0
+    for (index in 0..size) {
+        if (array.contains(index).not()) {
+            result = index
+        }
+    }
+    return result
+}
+
+fun findMostRepeatedNumber(array: Array<Int>): Int {
+    if (array.isEmpty()) return -1
+    val itemsMap = hashMapOf<Int, Int>()
+    array.forEach { item ->
+        if (itemsMap.containsKey(item)) {
+            val lastCount = itemsMap[item]!!.toInt()
+            itemsMap[item] = lastCount + 1
+        } else {
+            itemsMap[item] = 1
+        }
+    }
+    return itemsMap.maxBy { it.value }.key
 }
--- src/arrays/Main.kt
+++ src/arrays/Main.kt
@@ -7,4 +7,25 @@ fun main() {
 //    println(findMaximumTripletProduct(array = arrayOf(10, 3, 5, 6, 20)))
 //    println(findMaximumConsecutiveOnes(arrayOf(1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1)))
 //    println(findMaximumConsecutiveOnes(arrayOf(0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1)))
+//    println(isPalindrome("abcdba"))
+//    println(isNearlyPalindrome("abb5a"))
+//    println(isNearlyPalindrome("abba"))
+//    println(findMax(intArrayOf(1,2,3,10,4,5,13,12)))
+//    println(findMax(intArrayOf(-10, -20, -30, -40)))
+//    println(findSum(intArrayOf(1, 2, 3, 4, 5)))
+//    println(findSum(intArrayOf(100)))
+//    reverseArray(intArrayOf(1, 2, 3, 4, 5)).map {
+//        println(it)
+//    }
+//    println(containsDuplicate(array = intArrayOf(1, 2, 3, 4, 5)))
+//    println(containsDuplicate(array = intArrayOf(1, 2, 3, 4, 1)))
+//    println(removeDuplicate(intArrayOf(1, 2, 3, 4, 5)))
+//    println(removeDuplicate(intArrayOf(1, 1, 2, 2, 3, 3)))
+//    println(rotateAnArray(arrayOf(1, 2, 3, 4, 5), 2))
+//    println(rotateAnArray(arrayOf(10,20,30), 1))
+//    println(findFirstMissingNumberInSequence(arrayOf(1, 2, 4, 5),5))
+//    println(findFirstMissingNumberInSequence(arrayOf(5, 3, 4, 1),5))
+    println(findMostRepeatedNumber(arrayOf(3, 3, 4, 2, 4, 4, 2, 4, 4)))
+    println(findMostRepeatedNumber(arrayOf(3, 3, 4, 2, 4, 4, 2, 4)))
+
 }
